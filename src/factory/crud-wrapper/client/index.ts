89b32657f26ddd8ad47ad4280050198356d38646
gomzyyy <158289133+gomzyyy@users.noreply.github.com>
2026-04-07 19:37:52 +0530

    fix: types and default value for table behaviour in defineTable method

--- src/factory/crud-wrapper/client/index.ts
+++ src/factory/crud-wrapper/client/index.ts
@@ -1,6 +1,7 @@
 import { ClientWrapper } from "../../../core/index.js";
 import { GetTableOpts, TableBehaviour, UpdateTableOpts } from "../../../types/index.js";
 import { SupabaseClient } from "@supabase/supabase-js";
+import { getDefaultTableBehaviour } from "../../../core/crud-wrappers/client-wrapper/defaults.js";
 
 /**
  * @description This function is used to configure the client for a specific table.
@@ -9,8 +10,10 @@ import { SupabaseClient } from "@supabase/supabase-js";
  * @param behaviour The behaviour of the table.
  * @returns A ClientWrapper instance.
  */
-function defineTable<Table, TableFormData extends Partial<Table>, GetOptions extends GetTableOpts<Table>, UpdateOptions extends UpdateTableOpts<Table>>(supabase: SupabaseClient, tableName: string, behaviour: TableBehaviour<Table>): ClientWrapper<Table, TableFormData, GetOptions, UpdateOptions> {
+function defineTable<Table, TableFormData extends Partial<Table> = Partial<Table>, GetOptions extends GetTableOpts<Table> = GetTableOpts<Table>, UpdateOptions extends UpdateTableOpts<Table> = UpdateTableOpts<Table>>(supabase: SupabaseClient, tableName: string, behaviour: TableBehaviour<Table> = getDefaultTableBehaviour<Table>()): ClientWrapper<Table, TableFormData, GetOptions, UpdateOptions> {
     return new ClientWrapper<Table, TableFormData, GetOptions, UpdateOptions>(supabase, tableName, behaviour)
 }
+
+const users = defineTable<any>("" as any, "users")
 
 export { defineTable }
